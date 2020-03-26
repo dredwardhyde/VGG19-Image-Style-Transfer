c0f5f4113a911c3014dc3f134e9fa1fa00ad668f
edwardhyde <komesergey@gmail.com>
2020-03-26 21:08:09 +0300

    Initial commit

--- vgg_style_transfer.py
+++ vgg_style_transfer.py
@@ -1,13 +1,6 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 import tensorflow as tf
-
-import matplotlib.pyplot as plt
-import matplotlib as mpl
-
-mpl.rcParams['figure.figsize'] = (12, 12)
-mpl.rcParams['axes.grid'] = False
-
 import numpy as np
 import PIL.Image
 import time
@@ -50,40 +43,16 @@ def load_img(path_to_img):
     return img
 
 
-def imshow(image, title=None):
-    if len(image.shape) > 3:
-        image = tf.squeeze(image, axis=0)
-
-    plt.imshow(image)
-    if title:
-        plt.title(title)
-
-
 content_image = load_img(content_path)
 style_image = load_img(style_path)
 
-plt.subplot(1, 2, 1)
-imshow(content_image, 'Content Image')
-
-plt.subplot(1, 2, 2)
-imshow(style_image, 'Style Image')
-
-x = tf.keras.applications.vgg19.preprocess_input(content_image * 255)
-x = tf.image.resize(x, (224, 224))
-vgg = tf.keras.applications.VGG19(include_top=True, weights='imagenet')
-prediction_probabilities = vgg(x)
-predicted_top_5 = tf.keras.applications.vgg19.decode_predictions(prediction_probabilities.numpy())[0]
-
-print([(class_name, prob) for (number, class_name, prob) in predicted_top_5])
 
 vgg = tf.keras.applications.VGG19(include_top=False, weights='imagenet')
 
-print()
-for layer in vgg.layers:
-    print(layer.name)
-
 # Content layer where will pull our feature maps
-content_layers = ['block5_conv2']
+content_layers = ['block5_conv2',
+                  'block5_conv3',
+                  'block5_conv4']
 
 # Style layer of interest
 style_layers = ['block1_conv1',
@@ -99,24 +68,9 @@ num_style_layers = len(style_layers)
 def vgg_layers(layer_names):
     vgg = tf.keras.applications.VGG19(include_top=False, weights='imagenet')
     vgg.trainable = False
-
     outputs = [vgg.get_layer(name).output for name in layer_names]
-
     model = tf.keras.Model([vgg.input], outputs)
     return model
-
-
-style_extractor = vgg_layers(style_layers)
-style_outputs = style_extractor(style_image * 255)
-
-# Look at the statistics of each layer's output
-for name, output in zip(style_layers, style_outputs):
-    print(name)
-    print("  shape: ", output.numpy().shape)
-    print("  min: ", output.numpy().min())
-    print("  max: ", output.numpy().max())
-    print("  mean: ", output.numpy().mean())
-    print()
 
 
 def gram_matrix(input_tensor):
@@ -136,51 +90,17 @@ class StyleContentModel(tf.keras.models.Model):
         self.vgg.trainable = False
 
     def call(self, inputs):
-        "Expects float input in [0,1]"
         inputs = inputs * 255.0
         preprocessed_input = tf.keras.applications.vgg19.preprocess_input(inputs)
         outputs = self.vgg(preprocessed_input)
-        style_outputs, content_outputs = (outputs[:self.num_style_layers],
-                                          outputs[self.num_style_layers:])
-
-        style_outputs = [gram_matrix(style_output)
-                         for style_output in style_outputs]
-
-        content_dict = {content_name: value
-                        for content_name, value
-                        in zip(self.content_layers, content_outputs)}
-
-        style_dict = {style_name: value
-                      for style_name, value
-                      in zip(self.style_layers, style_outputs)}
-
+        style_outputs, content_outputs = (outputs[:self.num_style_layers], outputs[self.num_style_layers:])
+        style_outputs = [gram_matrix(style_output) for style_output in style_outputs]
+        content_dict = {content_name: value for content_name, value in zip(self.content_layers, content_outputs)}
+        style_dict = {style_name: value for style_name, value in zip(self.style_layers, style_outputs)}
         return {'content': content_dict, 'style': style_dict}
 
 
 extractor = StyleContentModel(style_layers, content_layers)
-
-results = extractor(tf.constant(content_image))
-
-style_results = results['style']
-
-print('Styles:')
-for name, output in sorted(results['style'].items()):
-    print("  ", name)
-    print("    shape: ", output.numpy().shape)
-    print("    min: ", output.numpy().min())
-    print("    max: ", output.numpy().max())
-    print("    mean: ", output.numpy().mean())
-    print()
-
-print("Contents:")
-for name, output in sorted(results['content'].items()):
-    print("  ", name)
-    print("    shape: ", output.numpy().shape)
-    print("    min: ", output.numpy().min())
-    print("    max: ", output.numpy().max())
-    print("    mean: ", output.numpy().mean())
-
-
 style_targets = extractor(style_image)['style']
 content_targets = extractor(content_image)['content']
 image = tf.Variable(content_image)
@@ -190,14 +110,9 @@ def clip_0_1(image):
   return tf.clip_by_value(image, clip_value_min=0.0, clip_value_max=1.0)
 
 
-opt = tf.optimizers.Adam(learning_rate=0.02, beta_1=0.99, epsilon=1e-1)
+opt = tf.optimizers.Adam(learning_rate=0.02, beta_1=0.99, epsilon=1e-3)
 
-# style_weight=0.0001
-# content_weight=5000
-#
-# total_variation_weight=30
-
-style_weight=0.01
+style_weight=0.001
 content_weight=15000
 
 total_variation_weight=50
